48ffe1004b8a5678c27cbb743fb59d302992fdfe
Sendya <18x@loacg.com>
2025-01-23 02:46:52 +0800

    fix: netns

--- container/containerd/containerd.go
+++ container/containerd/containerd.go
@@ -13,8 +13,9 @@ import (
 	"github.com/containerd/containerd/v2/pkg/cio"
 	"github.com/containerd/containerd/v2/pkg/namespaces"
 	"github.com/containerd/containerd/v2/pkg/oci"
-	gocni "github.com/containerd/go-cni"
+	"github.com/containerd/go-cni"
 	v1 "github.com/omalloc/container/api/v1"
+	"github.com/omalloc/container/pkg/idgen"
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
@@ -27,6 +28,8 @@ func New() (v1.Container, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	_ = os.MkdirAll("/var/lib/libnetwork/", os.ModeDir)
 
 	c := &Containerd{
 		client: client,
@@ -41,6 +44,7 @@ func New() (v1.Container, error) {
 
 func (c *Containerd) CreateContainer(opt *v1.ContainerOpts) error {
 	imageName := imageNameJoin(opt.Image, opt.Tag)
+
 	log.Printf("Trying load image %s", imageName)
 	image, err := c.imageLoad(imageName, true)
 	if err != nil {
@@ -49,57 +53,62 @@ func (c *Containerd) CreateContainer(opt *v1.ContainerOpts) error {
 
 	ctx := withCtx()
 	snapname := fmt.Sprintf("%s-snapshot", opt.Name)
+
 	container, err := c.client.NewContainer(
 		ctx,
-		opt.Name,
+		idgen.GenerateID(),
 		containerd.WithNewSnapshot(snapname, image),
 		containerd.WithNewSpec(
 			oci.WithImageConfig(image),
 			oci.WithHostNamespace(specs.PIDNamespace),
 			oci.WithProcessArgs("top"),
 		),
+		containerd.WithContainerLabels(map[string]string{
+			"Names": opt.Name, // 将 NAMES 添加为标签
+		}),
 	)
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
 	log.Printf("Successfully created container with ID %q and snapshot with ID %q", container.ID(), snapname)
-
-	libnetwork, err := gocni.New(
-		// one for loopback network interface
-		gocni.WithMinNetworkCount(2),
-		gocni.WithPluginConfDir("/etc/cni/net.d"),
-		gocni.WithPluginDir([]string{"/opt/cni/bin"}),
-		gocni.WithInterfacePrefix("eth"),
-	)
-	if err != nil {
-		log.Fatalf("failed to initialize cni library: %v", err)
-	}
-
-	if err := libnetwork.Load(gocni.WithLoNetwork, gocni.WithDefaultConf); err != nil {
-		log.Fatalf("failed to load cni configuration: %v", err)
-	}
-
-	labels := map[string]string{
-		"omalloc_pod_namespace":          "omalloc",
-		"omalloc_pod_name":               opt.Name,
-		"omalloc_pod_infra_container_id": container.ID(),
-		"IgnoreUnknown":                  "1",
-	}
-
-	// Setup network
-	result, err := libnetwork.Setup(withCtx(), container.ID(), libnetworkNamespace(opt.Name), gocni.WithLabels(labels))
-	if err != nil {
-		log.Fatalf("failed to setup network for namespace: %v", err)
-	}
-	// Get IP of the default interface
-	IP := result.Interfaces["eth0"].IPConfigs[0].IP.String()
-	fmt.Printf("IP of the default interface %s:%s", "eth0", IP)
 
 	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
 	if err != nil {
 		return err
 	}
 
+	libnetwork, err := cni.New(
+		// one for loopback network interface
+		cni.WithMinNetworkCount(1),
+		cni.WithPluginConfDir("/etc/cni/net.d"),
+		cni.WithPluginDir([]string{"/opt/cni/bin"}),
+		cni.WithInterfacePrefix("eth"),
+	)
+	if err != nil {
+		log.Fatalf("failed to initialize cni library: %v", err)
+		return err
+	}
+
+	netns, id := libnetworkNamespace(container.ID())
+
+	if err := libnetwork.Load(cni.WithConfListFile("/etc/cni/net.d/10-sbridge.conf")); err != nil {
+		log.Fatalf("failed to load cni network: %v", err)
+		return err
+	}
+
+	result, err := libnetwork.Setup(ctx, id, netns)
+	if err != nil {
+		log.Fatalf("failed to setup network for namespace: %v", err)
+	}
+	log.Printf("Successfully setup network for namespace: %s", netns)
+
+	for key, iff := range result.Interfaces {
+		if len(iff.IPConfigs) > 0 {
+			IP := iff.IPConfigs[0].IP.String()
+			fmt.Printf("IP of the interface %s:%s\n", key, IP)
+		}
+	}
 	return task.Start(ctx)
 }
 
@@ -214,7 +223,7 @@ func imageNameJoin(name, tag string) string {
 	return fmt.Sprintf("%s:%s", name, tag)
 }
 
-func libnetworkNamespace(name string) string {
-	_ = os.MkdirAll("/var/lib/libnetwork/", os.ModeDir)
-	return "/var/lib/libnetwork"
+func libnetworkNamespace(containerID string) (string, string) {
+	id := containerID[:10]
+	return fmt.Sprintf("/var/run/netns/%s", id), id
 }
